c9da683677461c4e68d708282ff5a0f0d4aa7447
Filip Skubacz <filip.skubacz00@gmail.com>
2024-06-05 18:43:45 +0200

    Move to shapenet overlays

--- datasets/nmr.py
+++ datasets/nmr.py
@@ -15,8 +15,11 @@ from utils.camera_utils import get_loop_cameras
 
 from .shared_dataset import SharedDataset
 
-NMR_DATASET_ROOT = None # Change this to your data directory
+NMR_DATASET_ROOT = (
+    "/storage/slurm/skubacz/NMR_Dataset/"  # Change this to your data directory
+)
 assert NMR_DATASET_ROOT is not None, "Update path of the dataset"
+
 
 class NMRDataset(SharedDataset):
     """
@@ -24,13 +27,11 @@ class NMRDataset(SharedDataset):
     Provides NMR renderings
     """
 
-    def __init__(self, 
-                 dataset_name="train",
-                 with_alpha=False
-                 ) -> None:
+    def __init__(self, cfg, dataset_name="train", with_alpha=False) -> None:
         super().__init__()
         self.cfg = cfg
         self.dataset_name = dataset_name
+        self.with_alpha = with_alpha
 
         # first check if the dataset is already on the local machine
         self.base_path = NMR_DATASET_ROOT
@@ -38,7 +39,9 @@ class NMRDataset(SharedDataset):
         list_prefix = "softras_"
 
         # list all categories
-        cats = [x for x in glob.glob(os.path.join(self.base_path, "*")) if os.path.isdir(x)]
+        cats = [
+            x for x in glob.glob(os.path.join(self.base_path, "*")) if os.path.isdir(x)
+        ]
 
         # Read the split for each category
         if dataset_name == "train":
@@ -56,8 +59,9 @@ class NMRDataset(SharedDataset):
         if dataset_name == "test":
             # src viewlist was copied from
             # https://github.com/sxyu/pixel-nerf/blob/91a044bdd62aebe0ed3a5685ca37cb8a9dc8e8ee/viewlist/src_dvr.txt
-            source_view_fname =  os.path.join(os.path.dirname(os.path.abspath(__file__)), 
-                                              "viewlist_src_nmr.txt")
+            source_view_fname = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "viewlist_src_nmr.txt"
+            )
             with open(source_view_fname, "r") as f:
                 src_view_list = f.readlines()
             self.src_view_dict = {}
@@ -76,12 +80,17 @@ class NMRDataset(SharedDataset):
                 base_dir = os.path.dirname(file_list)
                 cat = os.path.basename(base_dir)
                 with open(file_list, "r") as f:
-                    objs = [(cat, os.path.join(base_dir, x.strip())) for x in f.readlines()]
+                    objs = [
+                        (cat, os.path.join(base_dir, x.strip())) for x in f.readlines()
+                    ]
                 all_objs.extend(objs)
                 num_objs.append(len(objs))
 
-            print("Found {} categories, with {} files in them, respecively".format(
-                len(cats), num_objs))
+            print(
+                "Found {} categories, with {} files in them, respecively".format(
+                    len(cats), num_objs
+                )
+            )
 
         self.all_objs = all_objs
 
@@ -101,16 +110,18 @@ class NMRDataset(SharedDataset):
         self.imgs_per_obj_train = self.cfg.opt.imgs_per_obj
 
         self.projection_matrix = getProjectionMatrix(
-            znear=self.cfg.data.znear, zfar=self.cfg.data.zfar,
-            fovX=cfg.data.fov * 2 * np.pi / 360, 
-            fovY=cfg.data.fov * 2 * np.pi / 360).transpose(0,1)
+            znear=self.cfg.data.znear,
+            zfar=self.cfg.data.zfar,
+            fovX=cfg.data.fov * 2 * np.pi / 360,
+            fovY=cfg.data.fov * 2 * np.pi / 360,
+        ).transpose(0, 1)
 
         if cfg.data.subset != -1:
-            self.paths = self.paths[:cfg.data.subset]
+            self.paths = self.paths[: cfg.data.subset]
 
     def load_imgs_and_convert_cameras(self, rgb_paths, cam_path, num_views):
         """
-        Load the images, camera matrices and projection matrices for a given object 
+        Load the images, camera matrices and projection matrices for a given object
         """
         world_view_transforms = []
         view_world_transforms = []
@@ -122,24 +133,29 @@ class NMRDataset(SharedDataset):
 
         # validation dataset is used for scoring - fix cond frame for reproducibility
         # in trainng need to randomly sample the conditioning frame
-        if self.dataset_name == "val": 
+        if self.dataset_name == "val":
             indexes = torch.arange(num_views)
         elif self.dataset_name == "test":
             cat_id, obj_id, _ = cam_path.split("/")[-3:]
             src_idx = self.src_view_dict[cat_id][obj_id]
-            indexes = torch.tensor([src_idx] + [i for i in range(len(rgb_paths)) if i != src_idx])
+            indexes = torch.tensor(
+                [src_idx] + [i for i in range(len(rgb_paths)) if i != src_idx]
+            )
         else:
             indexes = torch.randperm(len(rgb_paths))[:num_views]
-            indexes = torch.cat([indexes[:self.cfg.data.input_images], indexes], dim=0)
+            indexes = torch.cat([indexes[: self.cfg.data.input_images], indexes], dim=0)
 
         for frame_idx in indexes:
 
             rgb_path = rgb_paths[frame_idx]
-            img = PILtoTorch(Image.open(rgb_path), (128,
-                                                    128))
+            img = PILtoTorch(Image.open(rgb_path), (128, 128))
 
             # Take replace the last "image" in rgb_path with "mask" to get the mask path
-            mask_path = rgb_path[:rgb_path.rfind("image")] + "mask" + rgb_path[rgb_path.rfind("image") + 5:]
+            mask_path = (
+                rgb_path[: rgb_path.rfind("image")]
+                + "mask"
+                + rgb_path[rgb_path.rfind("image") + 5 :]
+            )
             alpha_img = PILtoTorch(Image.open(mask_path), (128, 128))
             # Extend img with alpha channel
             if self.with_alpha:
@@ -171,9 +187,9 @@ class NMRDataset(SharedDataset):
             c2w_cmo = (
                 self._coord_trans_world
                 @ torch.tensor(c2w_cmo, dtype=torch.float32)
-                @ self._coord_trans_cam # to pixelnerf coordinate system
-                @ self._pixelnerf_to_colmap # to colmap coordinate system
-            ) 
+                @ self._coord_trans_cam  # to pixelnerf coordinate system
+                @ self._pixelnerf_to_colmap  # to colmap coordinate system
+            )
 
             # put in row-major order
             view_world_transform = c2w_cmo.transpose(0, 1)
@@ -189,14 +205,19 @@ class NMRDataset(SharedDataset):
         view_world_transforms = torch.stack(view_world_transforms)
         camera_centers = torch.stack(camera_centers)
 
-        full_proj_transforms = world_view_transforms.bmm(self.projection_matrix.unsqueeze(0).expand(
-            world_view_transforms.shape[0], 4, 4))
+        full_proj_transforms = world_view_transforms.bmm(
+            self.projection_matrix.unsqueeze(0).expand(
+                world_view_transforms.shape[0], 4, 4
+            )
+        )
 
-        return {"gt_images": imgs,
-                "world_view_transforms": world_view_transforms,
-                "view_to_world_transforms": view_world_transforms,
-                "full_proj_transforms": full_proj_transforms,
-                "camera_centers": camera_centers}
+        return {
+            "gt_images": imgs,
+            "world_view_transforms": world_view_transforms,
+            "view_to_world_transforms": view_world_transforms,
+            "full_proj_transforms": full_proj_transforms,
+            "camera_centers": camera_centers,
+        }
 
     def load_loop(self, rgb_paths, cam_path, num_imgs_in_loop):
         world_view_transforms = []
@@ -204,18 +225,28 @@ class NMRDataset(SharedDataset):
         camera_centers = []
         imgs = []
 
-        gt_imgs_and_cameras = self.load_imgs_and_convert_cameras(rgb_paths, cam_path, len(rgb_paths))
-        loop_cameras_c2w_cmo = get_loop_cameras(num_imgs_in_loop=num_imgs_in_loop, radius=2.73)
+        gt_imgs_and_cameras = self.load_imgs_and_convert_cameras(
+            rgb_paths, cam_path, len(rgb_paths)
+        )
+        loop_cameras_c2w_cmo = get_loop_cameras(
+            num_imgs_in_loop=num_imgs_in_loop, radius=2.73
+        )
 
         for src_idx in range(self.cfg.data.input_images):
             imgs.append(gt_imgs_and_cameras["gt_images"][src_idx])
             camera_centers.append(gt_imgs_and_cameras["camera_centers"][src_idx])
-            world_view_transforms.append(gt_imgs_and_cameras["world_view_transforms"][src_idx])
-            view_world_transforms.append(gt_imgs_and_cameras["view_to_world_transforms"][src_idx])
+            world_view_transforms.append(
+                gt_imgs_and_cameras["world_view_transforms"][src_idx]
+            )
+            view_world_transforms.append(
+                gt_imgs_and_cameras["view_to_world_transforms"][src_idx]
+            )
 
         for loop_camera_c2w_cmo in loop_cameras_c2w_cmo:
             view_world_transform = torch.from_numpy(loop_camera_c2w_cmo).transpose(0, 1)
-            world_view_transform = torch.from_numpy(loop_camera_c2w_cmo).inverse().transpose(0, 1)
+            world_view_transform = (
+                torch.from_numpy(loop_camera_c2w_cmo).inverse().transpose(0, 1)
+            )
             camera_center = view_world_transform[3, :3].clone()
 
             camera_centers.append(camera_center)
@@ -223,8 +254,12 @@ class NMRDataset(SharedDataset):
             view_world_transforms.append(view_world_transform)
 
             # use the closest camera as reference gt image
-            closest_gt_idx = torch.argmin(torch.norm(
-                gt_imgs_and_cameras["camera_centers"] - camera_center.unsqueeze(0), dim=-1)).item()
+            closest_gt_idx = torch.argmin(
+                torch.norm(
+                    gt_imgs_and_cameras["camera_centers"] - camera_center.unsqueeze(0),
+                    dim=-1,
+                )
+            ).item()
             imgs.append(gt_imgs_and_cameras["gt_images"][closest_gt_idx])
 
         imgs = torch.stack(imgs)
@@ -232,14 +267,19 @@ class NMRDataset(SharedDataset):
         view_world_transforms = torch.stack(view_world_transforms)
         camera_centers = torch.stack(camera_centers)
 
-        full_proj_transforms = world_view_transforms.bmm(self.projection_matrix.unsqueeze(0).expand(
-            world_view_transforms.shape[0], 4, 4))
+        full_proj_transforms = world_view_transforms.bmm(
+            self.projection_matrix.unsqueeze(0).expand(
+                world_view_transforms.shape[0], 4, 4
+            )
+        )
 
-        return {"gt_images": imgs,
-                "world_view_transforms": world_view_transforms,
-                "view_to_world_transforms": view_world_transforms,
-                "full_proj_transforms": full_proj_transforms,
-                "camera_centers": camera_centers}
+        return {
+            "gt_images": imgs,
+            "world_view_transforms": world_view_transforms,
+            "view_to_world_transforms": view_world_transforms,
+            "full_proj_transforms": full_proj_transforms,
+            "camera_centers": camera_centers,
+        }
 
     def __len__(self):
         return len(self.all_objs)
@@ -266,11 +306,17 @@ class NMRDataset(SharedDataset):
             num_views = len(rgb_paths)
 
         if self.dataset_name == "vis":
-            images_and_camera_poses = self.load_loop(rgb_paths, cam_path, 100)    
+            images_and_camera_poses = self.load_loop(rgb_paths, cam_path, 100)
         else:
-            images_and_camera_poses = self.load_imgs_and_convert_cameras(rgb_paths, cam_path, num_views)
+            images_and_camera_poses = self.load_imgs_and_convert_cameras(
+                rgb_paths, cam_path, num_views
+            )
 
-        images_and_camera_poses = self.make_poses_relative_to_first(images_and_camera_poses)
-        images_and_camera_poses["source_cv2wT_quat"] = self.get_source_cw2wT(images_and_camera_poses["view_to_world_transforms"])
+        images_and_camera_poses = self.make_poses_relative_to_first(
+            images_and_camera_poses
+        )
+        images_and_camera_poses["source_cv2wT_quat"] = self.get_source_cw2wT(
+            images_and_camera_poses["view_to_world_transforms"]
+        )
 
         return images_and_camera_poses
