682e194a56bc1bf059efe88a1206cb36deee7f51
Filip Skubacz <filip.skubacz00@gmail.com>
2024-06-05 18:49:49 +0200

    Add superimpose util fn

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -3,7 +3,7 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
@@ -15,8 +15,13 @@ from datetime import datetime
 import numpy as np
 import random
 
+import torchvision
+import torchvision.transforms.v2
+
+
 def inverse_sigmoid(x):
-    return torch.log(x/(1-x))
+    return torch.log(x / (1 - x))
+
 
 def PILtoTorch(pil_image, resolution):
     resized_image_PIL = pil_image.resize(resolution)
@@ -25,6 +30,7 @@ def PILtoTorch(pil_image, resolution):
         return resized_image.permute(2, 0, 1)
     else:
         return resized_image.unsqueeze(dim=-1).permute(2, 0, 1)
+
 
 def get_expon_lr_func(
     lr_init, lr_final, lr_delay_steps=0, lr_delay_mult=1.0, max_steps=1000000
@@ -61,6 +67,7 @@ def get_expon_lr_func(
 
     return helper
 
+
 def strip_lowerdiag(L):
     uncertainty = torch.zeros((L.shape[0], 6), dtype=torch.float, device="cuda")
 
@@ -72,8 +79,10 @@ def strip_lowerdiag(L):
     uncertainty[:, 5] = L[:, 2, 2]
     return uncertainty
 
+
 def strip_symmetric(sym):
     return strip_lowerdiag(sym)
+
 
 def standardize_quaternion(quaternions: torch.Tensor) -> torch.Tensor:
     """
@@ -89,6 +98,7 @@ def standardize_quaternion(quaternions: torch.Tensor) -> torch.Tensor:
         Standardized quaternions as tensor of shape (..., 4).
     """
     return torch.where(quaternions[..., 0:1] < 0, -quaternions, quaternions)
+
 
 def quaternion_raw_multiply(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     """
@@ -111,81 +121,88 @@ def quaternion_raw_multiply(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     oz = aw * bz + ax * by - ay * bx + az * bw
     return torch.stack((ow, ox, oy, oz), -1)
 
+
 # Matrix to quaternion does not come under NVIDIA Copyright
 # Written by Stan Szymanowicz 2023
 def matrix_to_quaternion(M: torch.Tensor) -> torch.Tensor:
     """
-    Matrix-to-quaternion conversion method. Equation taken from 
+    Matrix-to-quaternion conversion method. Equation taken from
     https://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
     Args:
         M: rotation matrices, (3 x 3)
     Returns:
         q: quaternion of shape (4)
     """
-    tr = 1 + M[ 0, 0] + M[ 1, 1] + M[ 2, 2]
+    tr = 1 + M[0, 0] + M[1, 1] + M[2, 2]
 
     if tr > 0:
         r = torch.sqrt(tr) / 2.0
-        x = ( M[ 2, 1] - M[ 1, 2] ) / ( 4 * r )
-        y = ( M[ 0, 2] - M[ 2, 0] ) / ( 4 * r )
-        z = ( M[ 1, 0] - M[ 0, 1] ) / ( 4 * r )
-    elif ( M[ 0, 0] > M[ 1, 1]) and (M[ 0, 0] > M[ 2, 2]):
-        S = torch.sqrt(1.0 + M[ 0, 0] - M[ 1, 1] - M[ 2, 2]) * 2 # S=4*qx 
-        r = (M[ 2, 1] - M[ 1, 2]) / S
+        x = (M[2, 1] - M[1, 2]) / (4 * r)
+        y = (M[0, 2] - M[2, 0]) / (4 * r)
+        z = (M[1, 0] - M[0, 1]) / (4 * r)
+    elif (M[0, 0] > M[1, 1]) and (M[0, 0] > M[2, 2]):
+        S = torch.sqrt(1.0 + M[0, 0] - M[1, 1] - M[2, 2]) * 2  # S=4*qx
+        r = (M[2, 1] - M[1, 2]) / S
         x = 0.25 * S
-        y = (M[ 0, 1] + M[ 1, 0]) / S 
-        z = (M[ 0, 2] + M[ 2, 0]) / S 
-    elif M[ 1, 1] > M[ 2, 2]: 
-        S = torch.sqrt(1.0 + M[ 1, 1] - M[ 0, 0] - M[ 2, 2]) * 2 # S=4*qy
-        r = (M[ 0, 2] - M[ 2, 0]) / S
-        x = (M[ 0, 1] + M[ 1, 0]) / S
+        y = (M[0, 1] + M[1, 0]) / S
+        z = (M[0, 2] + M[2, 0]) / S
+    elif M[1, 1] > M[2, 2]:
+        S = torch.sqrt(1.0 + M[1, 1] - M[0, 0] - M[2, 2]) * 2  # S=4*qy
+        r = (M[0, 2] - M[2, 0]) / S
+        x = (M[0, 1] + M[1, 0]) / S
         y = 0.25 * S
-        z = (M[ 1, 2] + M[ 2, 1]) / S
+        z = (M[1, 2] + M[2, 1]) / S
     else:
-        S = torch.sqrt(1.0 + M[ 2, 2] - M[ 0, 0] -  M[ 1, 1]) * 2 # S=4*qz
-        r = (M[ 1, 0] - M[ 0, 1]) / S
-        x = (M[ 0, 2] + M[ 2, 0]) / S
-        y = (M[ 1, 2] + M[ 2, 1]) / S
+        S = torch.sqrt(1.0 + M[2, 2] - M[0, 0] - M[1, 1]) * 2  # S=4*qz
+        r = (M[1, 0] - M[0, 1]) / S
+        x = (M[0, 2] + M[2, 0]) / S
+        y = (M[1, 2] + M[2, 1]) / S
         z = 0.25 * S
 
     return torch.stack([r, x, y, z], dim=-1)
 
+
 def build_rotation(r):
-    norm = torch.sqrt(r[:,0]*r[:,0] + r[:,1]*r[:,1] + r[:,2]*r[:,2] + r[:,3]*r[:,3])
+    norm = torch.sqrt(
+        r[:, 0] * r[:, 0] + r[:, 1] * r[:, 1] + r[:, 2] * r[:, 2] + r[:, 3] * r[:, 3]
+    )
 
     q = r / norm[:, None]
 
-    R = torch.zeros((q.size(0), 3, 3), device='cuda')
+    R = torch.zeros((q.size(0), 3, 3), device="cuda")
 
     r = q[:, 0]
     x = q[:, 1]
     y = q[:, 2]
     z = q[:, 3]
 
-    R[:, 0, 0] = 1 - 2 * (y*y + z*z)
-    R[:, 0, 1] = 2 * (x*y - r*z)
-    R[:, 0, 2] = 2 * (x*z + r*y)
-    R[:, 1, 0] = 2 * (x*y + r*z)
-    R[:, 1, 1] = 1 - 2 * (x*x + z*z)
-    R[:, 1, 2] = 2 * (y*z - r*x)
-    R[:, 2, 0] = 2 * (x*z - r*y)
-    R[:, 2, 1] = 2 * (y*z + r*x)
-    R[:, 2, 2] = 1 - 2 * (x*x + y*y)
+    R[:, 0, 0] = 1 - 2 * (y * y + z * z)
+    R[:, 0, 1] = 2 * (x * y - r * z)
+    R[:, 0, 2] = 2 * (x * z + r * y)
+    R[:, 1, 0] = 2 * (x * y + r * z)
+    R[:, 1, 1] = 1 - 2 * (x * x + z * z)
+    R[:, 1, 2] = 2 * (y * z - r * x)
+    R[:, 2, 0] = 2 * (x * z - r * y)
+    R[:, 2, 1] = 2 * (y * z + r * x)
+    R[:, 2, 2] = 1 - 2 * (x * x + y * y)
     return R
+
 
 def build_scaling_rotation(s, r):
     L = torch.zeros((s.shape[0], 3, 3), dtype=torch.float, device="cuda")
     R = build_rotation(r)
 
-    L[:,0,0] = s[:,0]
-    L[:,1,1] = s[:,1]
-    L[:,2,2] = s[:,2]
+    L[:, 0, 0] = s[:, 0]
+    L[:, 1, 1] = s[:, 1]
+    L[:, 2, 2] = s[:, 2]
 
     L = R @ L
     return L
 
+
 def safe_state(cfg, silent=False):
     old_f = sys.stdout
+
     class F:
         def __init__(self, silent):
             self.silent = silent
@@ -193,7 +210,14 @@ def safe_state(cfg, silent=False):
         def write(self, x):
             if not self.silent:
                 if x.endswith("\n"):
-                    old_f.write(x.replace("\n", " [{}]\n".format(str(datetime.now().strftime("%d/%m %H:%M:%S")))))
+                    old_f.write(
+                        x.replace(
+                            "\n",
+                            " [{}]\n".format(
+                                str(datetime.now().strftime("%d/%m %H:%M:%S"))
+                            ),
+                        )
+                    )
                 else:
                     old_f.write(x)
 
@@ -209,3 +233,13 @@ def safe_state(cfg, silent=False):
     torch.cuda.set_device(device)
 
     return device
+
+
+def superimpose_mask(input, mask):
+    # input_images = torchvision.transforms.RandomErasing(p=cfg.opt.mask_p)(input_images)
+    mask = torchvision.transforms.v2.RandomAffine(
+        degrees=180, translate=(0.40, 0.40), scale=(0.8, 1.2)
+    )(mask)
+    return (
+        input * (1 - mask[:, :, 3:4, ...]) + mask[:, :, :3, ...] * mask[:, :, 3:4, ...]
+    )
